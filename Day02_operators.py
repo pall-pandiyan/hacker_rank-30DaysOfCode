9f84a4ec2d583ab740545dc12ebc1d8999695b89
pall-pandiyan <pallpandiyan@gmail.com>
2022-05-18 12:12:13 +0530

    solved

--- Day02_operators.py
+++ Day02_operators.py
@@ -15,8 +15,14 @@ import sys
 #  3. INTEGER tax_percent
 #
 
+
 def solve(meal_cost, tip_percent, tax_percent):
     # Write your code here
+    tip = meal_cost*tip_percent/100
+    tax = meal_cost*tax_percent/100
+    total = meal_cost+tip+tax
+    print(round(total))
+
 
 if __name__ == '__main__':
     meal_cost = float(input().strip())
